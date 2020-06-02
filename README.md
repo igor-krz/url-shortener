4d4544e2d1be625824544f31dcc94db30e67409c
James Abustan <JimmyJammies@Jamess-MacBook-Pro.local>
2020-06-02 17:08:40 +0100

    database implemented sqlalchemy

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 * Initialise App - Server 
 * Create Database 
 * Link Database
+* Create Route
+
+
+
+#### To RUN
+* python3 -m venv env
+* source env/bin/activate
+* install sqlalchemy - pip install flask flask-sqlalchemy
+* check dependencies - pip list
+
+* to check DB  in terminal - sqlite3 db.sqlite3 / then .tables
--- app.py
+++ app.py
@@ -1,20 +1,30 @@
-from flask import Flask, make_response, render_template
-import sqlite3
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-# initialise app
+# Initialise app
 app = Flask(__name__)
 
-# Database 
-db = sqlite3.connect('database.db')
-print('Opened database successfully')
 
-db.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
-print ("Table created successfully")
-db.close()
+# Config
+# track modifications - warning messages don't appear when running
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
+# Connect Database 
+db = SQLAlchemy(app)
+
+# Create Model
+class URL(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    longUrl = db.Column(db.String)
+    shortUrl = db.Column(db.String)sqlite3 db.sqlite3
+
+# Routes
 @app.route('/')
-def home():
-    return render_template ('index.html')
+def index():
+    return render_template('index.html')
+
+
 
 
 if __name__ == '__main__':
