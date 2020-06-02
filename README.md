3c0b92ca357d88199be7346794ae44ac76ff91c4
James Abustan <JimmyJammies@Jamess-MacBook-Pro.local>
2020-06-02 16:26:09 +0100

    templates and app.py create

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # url-shortener
+
+### Steps Checklist :
+* Initialise App - Server 
+* Create Database 
+* Link Database
--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
-from flask import make_response, render_template
+from flask import Flask, make_response, render_template
+import sqlite3
 
-
+# initialise app
 app = Flask(__name__)
 
+# Database 
+db = sqlite3.connect('database.db')
+print('Opened database successfully')
+
+db.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
+print ("Table created successfully")
+db.close()
 
 @app.route('/')
 def home():
     return render_template ('index.html')
-
 
 
 if __name__ == '__main__':
